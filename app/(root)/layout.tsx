2793e8a6698944f239ec366f0209958ced11f99d
Titan <neeu.singh327@gmail.com>
2025-04-08 11:17:04 +0530

    Changed sign-in to signin

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const isUserAuthenticated=await isAuthenticated()
   if(!isUserAuthenticated){
-    redirect('/sign-in')
+    redirect('/signin')
   } 
   return (
   <div className="root-layout">
