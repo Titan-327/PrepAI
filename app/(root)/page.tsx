35495b876a465d0878a2ce50b3008c1f4681b65c
Titan <neeu.singh327@gmail.com>
2025-04-09 15:20:26 +0530

    TheGeneratedInterviewsAreShownOnHomepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,17 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { dummyInterviews } from "@/constants";
 import { InterviewCard } from "@/components/InterviewCard";
-export default function Home() {
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/auth.action";
+export default async function Home() {
+  const user=await getCurrentUser()
+  const [userInterviews,latestInterviews]=await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({userId:user?.id!})
+  ])
+
+
+  const hasPastInterviews=(userInterviews ?? []).length > 0;
+  const hasUpcomingInterviews=(latestInterviews ?? []).length > 0;
   return (
    <>
    <section className="card-cta">
@@ -21,17 +31,23 @@ export default function Home() {
    <section className="flex flex-col gap-6 mt-8">
 <h2>Your Interviews</h2>
 <div className="interviews-section">
-{dummyInterviews.map((interview) => (
+{hasPastInterviews ? (
+userInterviews?.map((interview) => (
 <InterviewCard {...interview} key={interview.id}/>
-))}
+))):(
+  <p>You haven&apos;t taken any interviews yet</p>
+)}
 </div>
    </section>
    <section className="flex flex-col gap-6 mt-8">
 <h2>Take an Interview</h2>
 <div className="interviews-section">
-{dummyInterviews.map((interview) => (
+{hasUpcomingInterviews ? (
+latestInterviews?.map((interview) => (
 <InterviewCard {...interview} key={interview.id}/>
-))}
+))):(
+  <p>There are no new interviews available</p>
+)}
 </div>
    </section>
    </>
