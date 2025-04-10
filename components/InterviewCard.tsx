046b84ac6b79d79e1273976233670aae4deeceb2
Titan <neeu.singh327@gmail.com>
2025-04-10 11:46:23 +0530

    ShownFeedBack

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { DisplayTechIcons } from './DisplayTechIcons';
 import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 export const InterviewCard = async ({id, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
-    const feedback=userId && id ? await getFeedbackByInterviewId({interviewId:id, userId}) 
+    const feedback=userId && id ? await getFeedbackByInterviewId({interviewId:id, userId}); 
     const normalizedType=/mix/gi.test(type) ? 'Mixed' : type
     const formattedDate=dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
     return(
